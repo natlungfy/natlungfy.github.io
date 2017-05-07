9d310baddb934eff1b3409092ffa7fa4199a8631
natlungfy <nat@natalielung.xyz>
2017-05-07 15:34:27 -0500

    update

--- projects/mobilegame/game.js
+++ projects/mobilegame/game.js
@@ -1,56 +1,84 @@
-window.onload = function() {
+/* global Phaser, gyro */
 
-	// game definition, 320x480
-	var game = new Phaser.Game(320,480,Phaser.CANVAS,"",{preload:onPreload, create:onCreate});                
+window.onload = function () {
 
-     // the player
-     var player;
-     var bgtile;
+    // game definition, 320x480
+    var game = new Phaser.Game(320, 480, Phaser.CANVAS, "", {preload: onPreload, create: onCreate, update: update, render: render});
 
-     // function executed on preload
-	function onPreload() {
-	    	game.load.image("player","mainCharacter/shoot_up.png");
-          game.load.image("bgtile", "office.jpg");	
-	}
+    // the player
+    var player;
+    var bgtile;
+    var bgtileAhead;
+    var bgsound;
 
-	// function to scale up the game to full screen
-	function goFullScreen(){
-		game.scale.pageAlignHorizontally = true;
-		game.scale.pageAlignVertically = true;
-		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		game.scale.setScreenSize(true);
-	}
+    // function executed on preload
+    function onPreload() {
+        game.load.image("player", "mainCharacter/shoot_up.png");
+        game.load.image("bgtile", "office.png");
+        game.load.audio("bgsound", "sounds/mainBackground.mp3")
+    }
 
-	// function to be called when the game has been created
-	function onCreate() {
-          // initializing physics system
-          game.physics.startSystem(Phaser.Physics.ARCADE);
-		// going full screen
-          goFullScreen();
-          // adding the player on stage
-          player = game.add.sprite(160,240,"player");
-          //bgtile = game.add.tileSprite(0,0,game.stage.bounds.width, game.cache.getImage('bgtile').height, 'bgtile');
-          //change background color
-           game.stage.backgroundColor = '#fff7af';
-          // setting player anchor point
-          player.anchor.setTo(0.5,-1.5);
-          // enabling physics car.body.collideWorldBounds = true;
-          game.physics.enable(player, Phaser.Physics.ARCADE);
-          // the player will collide with bounds
-          player.body.collideWorldBounds = true;
-          // setting player bounce
-          player.body.bounce.set(0.0);
-	     // setting gyroscope update frequency
-          gyro.frequency = 5;
-		// start gyroscope detection
-          gyro.startTracking(function(o) {
-               // updating player velocity
-               player.body.velocity.x += o.gamma/20;
-               player.body.velocity.y += o.beta/20;
-          });		
-	}
+    // function to scale up the game to full screen
+    function goFullScreen() {
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.scale.setScreenSize(true);
+    }
 
-     // function update() {
-     //      bgtile.tilePosition.x -= 1;
-     // }
-}
+    // function to be called when the game has been created
+    function onCreate() {
+        // initializing physics system
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        // going full screen
+        goFullScreen();
+
+        bgtile = game.add.sprite(game.world.centerX, game.world.centerY, "bgtile");
+        bgtile.anchor.setTo(0.5, 0.5);
+        bgtileAhead = game.add.sprite(game.world.centerX, -game.world.centerY, "bgtile");
+        bgtileAhead.anchor.setTo(0.5, 0.5);
+
+        bgsound = game.add.audio('bgsound',1,true); //true means looping is enabled.
+
+        // adding the player on stage
+        player = game.add.sprite(160, 240, "player");
+        //bgtile = game.add.tileSprite(0,0,game.stage.bounds.width, game.cache.getImage('bgtile').height, 'bgtile');
+        
+        //change background color
+        //game.stage.backgroundColor = '#fff7af';
+
+        // setting player anchor point
+        player.anchor.setTo(0.5, -1.5);
+        // enabling physics car.body.collideWorldBounds = true;
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        // the player will collide with bounds
+        player.body.collideWorldBounds = true;
+        // setting player bounce
+        player.body.bounce.set(0.0);
+        // setting gyroscope update frequency
+        gyro.frequency = 5;
+        // start gyroscope detection
+        gyro.startTracking(function (o) {
+            // updating player velocity
+            //player.body.velocity.x += o.gamma / 20; // TODO, CHANGE THIS
+            //player.body.velocity.y += o.beta / 20;
+            player.x = o.gamma; // TODO, CHANGE THIS??
+            player.y = o.beta;
+        });
+    }
+
+    function update() {
+        bgtile.y += 2;
+        bgtileAhead.y += 2;
+        if(bgtile.y > game.world.centerY * 3 - 1){
+            bgtile.y = -game.world.centerY + 1;
+        }
+        if(bgtileAhead.y > game.world.centerY * 3 - 1){
+            bgtileAhead.y = -game.world.centerY + 1;
+        }
+    }
+    
+    function render() {
+        game.debug.spriteInfo(player, 32, 32);
+    }
+};
