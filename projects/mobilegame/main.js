cb9fd5a919cce98587e213e523912ed4e99470b5
natlungfy <nat@natalielung.xyz>
2017-05-08 17:17:15 -0500

    update

--- projects/mobilegame/main.js
+++ projects/mobilegame/main.js
@@ -1,8 +1,8 @@
 var OfficeEscape = OfficeEscape || {}; 
 OfficeEscape.game = new Phaser.Game(320, 480, Phaser.AUTO, '');
-SpaceHipster.game.state.add('Boot', OfficeEscape.Boot);
-SpaceHipster.game.state.add('Preload', OfficeEscape.Preload);
-SpaceHipster.game.state.add('MainMenu', OfficeEscape.MainMenu);
-SpaceHipster.game.state.add('Game', OfficeEscape.Game);
+OfficeEscape.game.state.add('Boot', OfficeEscape.Boot);
+OfficeEscape.game.state.add('Preload', OfficeEscape.Preload);
+OfficeEscape.game.state.add('MainMenu', OfficeEscape.MainMenu);
+OfficeEscape.game.state.add('Game', OfficeEscape.Game);
  
-SpaceHipster.game.state.start('Boot');
+OfficeEscape.game.state.start('Boot');
